48d7bd31a94c6cafa16698163c005a455d359b32
iuliadogar23 <iuliabaaam@gmail.com>
2020-01-07 14:46:44 +0200

    changed google services json

--- app/src/main/java/com/project/meetme/MainActivity.java
+++ app/src/main/java/com/project/meetme/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.database.FirebaseDatabase;
 import com.project.meetme.model.User;
 import com.project.meetme.repos.UserRepository;
 
@@ -11,9 +12,12 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        UserRepository userRepository = new UserRepository();
-        userRepository.create("ana", "doggy@gmail.com", "test", 07L);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        UserRepository userRepository = new UserRepository(database);
+        userRepository.getAll();
+//        userRepository.create("ana", "doggy@gmail.com", "test", 07L);
     }
 }
--- app/src/main/java/com/project/meetme/repos/UserRepository.java
+++ app/src/main/java/com/project/meetme/repos/UserRepository.java
@@ -1,35 +1,45 @@
 package com.project.meetme.repos;
 
+import android.util.Log;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.project.meetme.model.User;
+
+import static android.content.ContentValues.TAG;
 
 public class UserRepository {
 
-    private DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
-//
-//    private void getAll() {
-//        databaseReference = FirebaseDatabase.getInstance().getReference("users");
-//
-//        databaseReference.addValueEventListener(new ValueEventListener() {
-//            @Override
-//            public void onDataChange(DataSnapshot dataSnapshot) {
-//                // This method is called once with the initial value and again
-//                // whenever data at this location is updated.
-//                String value = dataSnapshot.getValue(String.class);
-//                Log.d(TAG, "Value is: " + value);
-//            }
-//
-//            @Override
-//            public void onCancelled(DatabaseError error) {
-//                // Failed to read value
-//                Log.w(TAG, "Failed to read value.", error.toException());
-//            }
-//        });
-//    }
+    private DatabaseReference databaseReference;
+
+    public UserRepository(FirebaseDatabase database) {
+        this.databaseReference = database.getReference("users");
+    }
+
+
+    public void getAll() {
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                Log.d(TAG, "Value is: " + value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
+    }
 
     public void create(String name, String email, String password, Long phone) {
         User user = new User(name, email, password, phone);
-        databaseReference.setValue(user);
+        databaseReference.child("users").setValue(user);
     }
 }
