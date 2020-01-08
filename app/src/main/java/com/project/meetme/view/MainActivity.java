c74a32c0846c3508984d0d4180c62897b5fa2da0
iuliadogar23 <iuliabaaam@gmail.com>
2020-01-08 16:27:32 +0200

    login, register

---
+++ app/src/main/java/com/project/meetme/view/MainActivity.java
@@ -0,0 +1,39 @@
+package com.project.meetme.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.project.meetme.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button login;
+    Button register;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        login = findViewById(R.id.loginButton);
+        register = findViewById(R.id.registerButton);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+            }
+        });
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, RegisterActivity.class));
+            }
+        });
+    }
+}
--- app/src/main/java/com/project/meetme/view/RegisterActivity.java
+++ app/src/main/java/com/project/meetme/view/RegisterActivity.java
@@ -14,45 +14,56 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.project.meetme.R;
+import com.project.meetme.model.User;
+import com.project.meetme.repos.UserRepository;
 
-public class LoginActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity {
 
     EditText etEmail;
     EditText etPassword;
-    Button login;
+    Button register;
 
-    FirebaseAuth firebaseAuth;
+    private FirebaseAuth firebaseAuth;
+    private DatabaseReference databaseReference;
+    private UserRepository userRepository;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_register);
 
-        etEmail = findViewById(R.id.etEmailLogin);
-        etPassword = findViewById(R.id.etPasswordLogin);
-        login = findViewById(R.id.buttonLogin);
+        userRepository = new UserRepository();
+
+        etEmail = findViewById(R.id.etEmailRegister);
+        etPassword = findViewById(R.id.etPasswordRegister);
+        register = findViewById(R.id.buttonRegister);
 
         firebaseAuth = FirebaseAuth.getInstance();
 
-        login.setOnClickListener(new View.OnClickListener() {
+        register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 final String email = etEmail.getText().toString();
                 final String password = etPassword.getText().toString();
 
-                firebaseAuth.signInWithEmailAndPassword(email, password)
-                        .addOnCompleteListener((new OnCompleteListener<AuthResult>() {
+                firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(
+                        new OnCompleteListener<AuthResult>() {
                             @Override
                             public void onComplete(@NonNull Task<AuthResult> task) {
                                 if (task.isSuccessful()) {
-                                    startActivity(new Intent(LoginActivity.this, ProfileActivity.class));
+                                    User user = new User(null, email, password, null);
+                                    userRepository.create(user);
+                                    startActivity(new Intent(RegisterActivity.this, ProfileActivity.class));
                                 } else {
-                                    Toast.makeText(LoginActivity.this, task.getException().getMessage(),
+                                    Toast.makeText(RegisterActivity.this, task.getException().getMessage(),
                                             Toast.LENGTH_LONG).show();
                                 }
                             }
-                        }));
+                        }
+                );
             }
         });
     }
